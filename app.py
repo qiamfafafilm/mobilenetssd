6458c2eb527397df67e73e0abd2460c15247571a
Nameezer <89238677+Nameezer@users.noreply.github.com>
2021-11-18 11:38:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -159,8 +159,8 @@ def event_handle(event):
             replyObj = TextSendMessage(text="อยู่ในใจเสมอ")
             line_bot_api.reply_message(rtoken, replyObj)
         elif (msg == "เอาไปเลย1นิ้วโป้ง") :
-            headers = request.headers
-            json_headers = json.dumps({k:v for k, v in headers.items()})
+            #headers = request.headers
+            #json_headers = json.dumps({k:v for k, v in headers.items()})
             '''
             json_line = request.get_json(force=False,cache=False)
             json_line = json.dumps(json_line)
